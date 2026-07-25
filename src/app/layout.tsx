0c44452f2d6695a017bfa83dee406282ce4fc59d
Jose-Murilo <murilojucas360@hotmail.com>
2026-07-25 13:27:28 -0300

    feat: create home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,15 @@ export default function RootLayout({
       lang="pt-BR"
       className={cn("h-full", "antialiased", "font-sans ", inter.variable)}
     >
-      <body className="bg-gray-900 text-white">{children}</body>
+      <body className="bg-gray-900 text-white">
+        <section className="flex h-screen">
+          <main className="relative flex-1 overflow-auto min-w-0">
+            <div className="p-4 sm:p-6 md:p-8 max-w-full md:max-w-3xl mx-auto h-full">
+              {children}
+            </div>
+          </main>
+        </section>
+      </body>
     </html>
   );
 }
