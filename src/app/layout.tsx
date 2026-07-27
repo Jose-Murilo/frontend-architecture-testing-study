623757bde123d6a9cf8d3fc5b98197883e8d796f
Jose-Murilo <murilojucas360@hotmail.com>
2026-07-26 23:44:10 -0300

    feat: adding import of sidebar in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Sidebar } from "@/components/sidebar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
     >
       <body className="bg-gray-900 text-white">
         <section className="flex h-screen">
+          <Sidebar />
           <main className="relative flex-1 overflow-auto min-w-0">
             <div className="p-4 sm:p-6 md:p-8 max-w-full md:max-w-3xl mx-auto h-full">
               {children}
